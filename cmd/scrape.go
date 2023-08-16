00f913fa7ddde0eda46462bffedc2ac6fc00d81c
raul cubas <raulcubas2016@gmail.com>
2023-08-16 22:12:15 +0100

    :fire: revome comments and add little short

--- cmd/scrape.go
+++ cmd/scrape.go
@@ -13,7 +13,7 @@ import (
 // scrapeCmd represents the scrape command
 var scrapeCmd = &cobra.Command{
 	Use:   "scrape",
-	Short: "Scrape emojis from gitmoji.dev and enrich your commits.",
+	Short: "Scrape emojis from gitmoji.dev.",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("scrape called")
@@ -29,14 +29,4 @@ var scrapeCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(scrapeCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// scrapeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// scrapeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
